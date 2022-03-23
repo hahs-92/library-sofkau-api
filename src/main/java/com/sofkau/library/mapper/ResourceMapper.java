b283df7322c2d732072bc6c3dc6353f4e20da614
Alex <alexandre_2420@outlook.com>
2022-03-23 17:13:47 -0500

    added: ResourceMapper

---
+++ src/main/java/com/sofkau/library/mapper/ResourceMapper.java
@@ -0,0 +1,50 @@
+package com.sofkau.library.mapper;
+
+import com.sofkau.library.dto.ResourceDto;
+import com.sofkau.library.model.Resource;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ResourceMapper {
+
+    public Resource fromDTO(ResourceDto dto) {
+        Resource resource = new Resource();
+        resource.setId(dto.getId());
+        resource.setTitle(dto.getTitle());
+        resource.setDescription(dto.getDescription());
+        resource.setType(dto.getType());
+        resource.setTheme(dto.getTheme());
+
+        return resource;
+    }
+
+    public ResourceDto fromCollection(Resource resource) {
+        ResourceDto resourceDto = new ResourceDto();
+        resourceDto.setId(resource.getId());
+        resourceDto.setTitle(resourceDto.getTitle());
+        resourceDto.setDescription(resource.getDescription());
+        resourceDto.setType(resourceDto.getType());
+        resourceDto.setTheme(resource.getTheme());
+
+        return resourceDto;
+    }
+
+
+    public List<ResourceDto> fromCollectionList(List<Resource> list) {
+        if(list == null) {
+            return  null;
+        }
+
+        List<ResourceDto> resourceDtoList = new ArrayList<>(list.size());
+        Iterator listTracks = list.iterator();
+
+        while(listTracks.hasNext()) {
+            Resource resource = (Resource) listTracks.next();
+            resourceDtoList.add(fromCollection(resource));
+        }
+
+        return resourceDtoList;
+    }
+}
--- src/main/java/com/sofkau/library/service/ResourceService.java
+++ src/main/java/com/sofkau/library/service/ResourceService.java
@@ -1,4 +1,12 @@
 package com.sofkau.library.service;
 
+import com.sofkau.library.repository.IResourceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ResourceService {
+    @Autowired
+    private IResourceRepository iResourceRepository;
+
 }
